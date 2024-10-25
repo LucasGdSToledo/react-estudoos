887f92ed99f9737152d0680488846b290f9ebb7a
LucasGdSToledo <164894386+LucasGdSToledo@users.noreply.github.com>
2024-10-25 22:46:20 +0000

    useState

--- miauw/src/Home.js
+++ miauw/src/Home.js
@@ -1,13 +1,25 @@
+import {useState} from 'react';
+
 function Home(){
+    // velho const nome = "mario";
+    //const [nome, setNome(mudar alguma coisa)] = useState(); novo
+
+    const [nome, setNome] = useState('mario');
+    const [idade, setIdade] = useState( 20);
+
     const mensagemAoClickar = function (){
-        console.log("harou evrinhone ");
+        setNome("luiiiiiiiigi");
+        setIdade(24);
+
     }
+    
 
 
     return(
         <div className = "home">
             <h2>home page</h2>
             <button onClick={mensagemAoClickar}>click me</button>
+            <p>{nome}, tem {idade} anos</p>
         </div>
     );
 }
